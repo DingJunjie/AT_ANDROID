8f15e93b67e6c1c4ca49c54a9ac9b48581707fa5
wen <3034827341@qq.com>
2024-08-09 17:35:14 +0800

    修改array默认值

--- app/src/main/java/com/bitat/utils/JsonUtils.kt
+++ app/src/main/java/com/bitat/utils/JsonUtils.kt
@@ -11,10 +11,11 @@ import java.io.InputStream
 //封装序列化工具
 @OptIn(ExperimentalSerializationApi::class)
 object JsonUtils {
-    inline fun <reified R> fromJsonStr(json: String): R = Json.decodeFromString(json)
-    inline fun <reified T> toJsonStr(data: T): String = Json.encodeToString(data)
-    inline fun <reified R> fromJson(json: InputStream): R = Json.decodeFromStream(json)
+    val instance = Json { encodeDefaults = true }
+    inline fun <reified R> fromJsonStr(json: String): R = instance.decodeFromString(json)
+    inline fun <reified T> toJsonStr(data: T): String = instance.encodeToString(data)
+    inline fun <reified R> fromJson(json: InputStream): R = instance.decodeFromStream(json)
     inline fun <reified T> toJson(data: T): ByteArray = ByteArrayOutputStream().also {
-        Json.encodeToStream(data, it)
+        instance.encodeToStream(data, it)
     }.toByteArray()
 }
