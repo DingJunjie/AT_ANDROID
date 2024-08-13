098756b1089b2505e8194d40c767874d4cd12cd1
wen <3034827341@qq.com>
2024-08-13 19:15:43 +0800

    封装朋友列表

--- app/src/main/java/com/bitat/repository/common/CuRes.kt
+++ app/src/main/java/com/bitat/repository/common/CuRes.kt
@@ -12,11 +12,11 @@ value class CuRes<T>(val v: Any) {
     fun get(): T = if (v is CodeErr) throwMsg("CodeErr:${v.code} ~ ${v.msg}") else v as T
     fun getOr(t: T): T = if (v is CodeErr) t else v as T
     inline fun getElse(fn: () -> T): T = if (v is CodeErr) fn() else v as T
-    inline fun map(fn: (T) -> Unit) = this.apply {
+    inline fun map(fn: (T) -> Unit) = apply {
         if (v !is CodeErr) fn(v as T)
     }
 
-    inline fun errMap(fn: (CodeErr) -> Unit) = this.apply {
+    inline fun errMap(fn: (CodeErr) -> Unit) = apply {
         if (v is CodeErr) fn(v)
     }
 
