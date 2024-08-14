ccc20c862dbf1a6654f2c74c1bce372a7d084af0
wen <3034827341@qq.com>
2024-08-14 16:05:29 +0800

    封装朋友列表

--- app/src/main/java/com/bitat/repository/common/CuRes.kt
+++ app/src/main/java/com/bitat/repository/common/CuRes.kt
@@ -12,14 +12,8 @@ value class CuRes<T>(val v: Any) {
     fun get(): T = if (v is CodeErr) throwMsg("CodeErr:${v.code} ~ ${v.msg}") else v as T
     fun getOr(t: T): T = if (v is CodeErr) t else v as T
     inline fun getElse(fn: () -> T): T = if (v is CodeErr) fn() else v as T
-    inline fun map(fn: (T) -> Unit) = apply {
-        if (v !is CodeErr) fn(v as T)
-    }
-
-    inline fun errMap(fn: (CodeErr) -> Unit) = apply {
-        if (v is CodeErr) fn(v)
-    }
-
+    inline fun map(fn: (T) -> Unit) = apply { if (v !is CodeErr) fn(v as T) }
+    inline fun errMap(fn: (CodeErr) -> Unit) = apply { if (v is CodeErr) fn(v) }
     private fun throwMsg(msg: String): Nothing = throw RuntimeException(msg)
 
     companion object {
