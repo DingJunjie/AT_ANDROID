19ab185a6134a182f62f7736de260837fd5fc8bd
lee <876794356@qq.com>
2024-08-26 11:44:19 +0800

    封装朋友列表

---
+++ app/src/main/java/com/bitat/repository/po/SingleMsgPo.kt
@@ -0,0 +1,23 @@
+package com.bitat.repository.po
+
+import android.database.Cursor
+
+public class SingleMsgPo {
+    var selfId: Long = 0
+    var otherId: Long =0
+    var status: Short =0
+    var time: Long =0
+    var kind: Short =0
+    var content: String =""
+
+    companion object {
+        fun of(cursor: Cursor) = SingleMsgPo().apply {
+            selfId = cursor.getLong(0)
+            otherId = cursor.getLong(1)
+            status = cursor.getShort(2)
+            time = cursor.getLong(3)
+            kind = cursor.getShort(4)
+            content = cursor.getString(5)
+        }
+    }
+}
--- app/src/main/java/com/bitat/repository/sqlDB/SingleMsgDB.kt
+++ app/src/main/java/com/bitat/repository/sqlDB/SingleMsgDB.kt
@@ -1,6 +1,9 @@
 package com.bitat.repository.sqlDB
 
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import com.bitat.repository.po.SingleMsgPo
+import com.bitat.repository.po.UserPo
 
 private const val CREATE_TABLE_SINGLE_MSG = """
 CREATE TABLE IF NOT  EXISTS "single_msg" (
@@ -25,4 +28,15 @@ ON "single_msg" (
 """
 object SingleMsgDB {
     fun init(db: SQLiteDatabase) = db.execSQL(CREATE_TABLE_SINGLE_MSG)
+    fun findMsg(selfId: Long,otherId:Long,pageSize:Byte) =SqlDB.queryBatch(SingleMsgPo::of,
+        "select * form single_msg where self_id = ? and other_id = ? order by time limit ?", selfId,otherId,pageSize)
+    fun getMsg(selfId: Long,otherId:Long) =SqlDB.queryOne(SingleMsgPo::of,
+        "select * form single_msg where self_id = ? and other_id = ? order by time limit 1", selfId,otherId)
+    fun insertOne(selfId:Long,otherId:Long,status:Short,time:Long,kind:Short,content:String) =
+        SqlDB.query("insert into single_msg (self_id,other_id,status,time,kind,content) values (?,?,?,?,?,?);", selfId,otherId,status,time,kind,content)
+    fun updateKind(kind:Short,selfId: Long,otherId:Long,content: String,time: Long) =
+        SqlDB.query("update single_msg set kind = ? where self_id = ? and other_id = ? and content = ? and time = ?",kind,selfId,otherId,content,time)
+    fun delete(selfId: Long,otherId:Long,content: String,time: Long) =
+        SqlDB.query("delete from single_msg where self_id = ? and other_id = ? and content = ? and time = ?",selfId,otherId,content,time)
 }
+
