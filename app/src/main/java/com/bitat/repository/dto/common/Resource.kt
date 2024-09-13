f21742ffbc136c534497202594ce4c98256ec9ea
lee <876794356@qq.com>
2024-09-13 16:19:20 +0800

    微调

--- app/src/main/java/com/bitat/repository/dto/common/Resource.kt
+++ app/src/main/java/com/bitat/repository/dto/common/Resource.kt
@@ -5,8 +5,8 @@ import kotlinx.serialization.Serializable
 //博文资源类
 @Serializable
 class ResourceDto {
-    var video: String = "" //视频
     var images: Array<String> = emptyArray() //图片
+    var video: String = "" //视频
     var audio: String = ""
     var richText: String = "" //富文本
 }
